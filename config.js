f242689886baf858ffbe44a40f8b8956af7659cc
blueskyfre <chphed@gmail.com>
2026-05-21 17:16:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var Config = (function() {
   'use strict';
   return {
-    SS_ID          : '1f14LDZ4EWPokoRswVRNVcqh71K3VOCLO_XNCWni8MVY',
+    SS_ID          : '1OgVY0IJOGYfl2qv5eeOaaDehVpQFWcSJGJV4PCDoWoA',
     DEPLOY         : 'https://script.google.com/macros/s/AKfycbxcoYt2Ifqwv09zZbwJT7na9Km0RslxaOdi8un22wt8BqIeJBt44KRo5faoC6Rr4V3gkw/exec',
     GITHUB         : 'https://blueskyfre.github.io/consult3gra2/',
     BACKGROUND_IMG : 'https://blueskyfre.github.io/chphed.github.io/back.png'
