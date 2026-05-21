26c610b1f627e1844e9c306b2b56c3ee25c665a2
blueskyfre <chphed@gmail.com>
2026-05-21 18:48:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var Config = (function() {
   'use strict';
   return {
     SS_ID          : '1OgVY0IJOGYfl2qv5eeOaaDehVpQFWcSJGJV4PCDoWoA',
-    DEPLOY         : 'https://script.google.com/macros/s/AKfycbxcoYt2Ifqwv09zZbwJT7na9Km0RslxaOdi8un22wt8BqIeJBt44KRo5faoC6Rr4V3gkw/exec',
-    GITHUB         : 'https://blueskyfre.github.io/consult3gra2/',
+    DEPLOY         : 'https://script.google.com/macros/s/AKfycbxMno4zxec4f5-vyxImb9XDQlzwStZVqiFQl0M6Y1m6NCwAFl-9d1L2sDlgBtQtNrxX/exec',
+    GITHUB         : 'https://blueskyfre.github.io/consult3gra2',
     BACKGROUND_IMG : 'https://blueskyfre.github.io/chphed.github.io/back.png'
   };
 })();
